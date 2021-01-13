0bd5c91898d86361c4650011a1f1f2161218e04a
paulinaweterle <76213919+paulinaweterle@users.noreply.github.com>
2021-01-13 22:04:56 +0100

    Add files via upload

--- archiwum.sh
+++ archiwum.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+ARCHIVE_PATH=/home/s96462/archiwum
+DATE=date +%d-%m-%y
+arg=$#
+if [ "$#" = 1 ]; then
+    echo "argument $1"
+    if [ -d "$1" ]; then
+        mv "$1" "$1-$DATE"
+        cp -r "$1-$DATE" "$ARCHIVE_PATH" 
+        mv "$1-$DATE" "$1"
+    fi
+fi
